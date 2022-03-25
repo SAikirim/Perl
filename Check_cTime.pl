f249a3dfb84a3de0508df6907368ba60f8f4f399
JongCheol Yim <kyj9724@ssrinc.co.kr>
2022-03-25 17:32:58 +0900

    add,update 220325_17:32

--- Check_cTime.pl
+++ Check_cTime.pl
@@ -3,12 +3,9 @@ use strict;
 use warnings;
 
 
-#system("dir") > ;
-#my @in_filename ;
-#print @in_filename;
-#my @in_name = split(" ",@in_filename);
 #
-#print @in_name;
+# 최근에 수정된 파일 찾기(cTime)
+#
 
 my $old_age = 0;
 my $old_name;
@@ -27,14 +24,3 @@ while (<*>) {
 }
 #my ($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $size, $atime, $mtime, $ctime, $blksize, $blocks) = stat("README.md");
 printf "\nold filename: %s\nold_age: %d", $old_name, $old_age;
-
-
-
-__DATA__
-red green  blue
-red
-blue
-green
-red
-blue
-blue
