f05c9ff1ef894c41922ba27935fd1e8b07b7338b
JongCheol Yim <kyj9724@ssrinc.co.kr>
2022-04-20 17:39:08 +0900

    [add] mv 커맨드 코딩및 sprintf함수 작동방식 확인

--- Text.pl
+++ Text.pl
@@ -3,36 +3,10 @@ use strict;
 use warnings;
 
 #
-# grep 사용 예시
-# grep을 사용해 배열(@foundFtpUsers)의 존재 여부를 확인할 수 있다. 
-# 참이면 배열값을 리턴
-
-my $test = "/test/test.txt";
-my $basetest = $test =~ /[^\/\n\t]+$/ ;
-
-my ($old_name, $new_name) = @ARGV;
-if ( !defined ($old_name && $new_name)  ) {
-    print "인수를 넣어주세요";
-    exit;
-}
-$old_name =~ s/\\/\//g ;
-$new_name =~ s/\\/\//g ;
-
-if (-e $old_name) {
-    if ( -d $new_name ) {
-        (my $base_name = $old_name) =~ /[^\/\n\t]+$/ ;
-        $new_name .= "/$base_name";
-    }
-    
-    rename ( $old_name, $new_name ) || die "rename failed :$!\n";
-    }
-else {      
-    print "NO Fire or Diretory : $!";
-    exit;
-}
-
-
-
+# mv 명령어
+# mv 처럼 사용, 옵션 없음, 인수 2개를 받음 
+# 2번째 인수가 디렉터리면 그 디렉터리 밑에 파일 생성
+#
 
 opendir (GIT, "C:/_Git")  || die "no git : $!\n";
 while ( my $name = readdir(GIT)) {
